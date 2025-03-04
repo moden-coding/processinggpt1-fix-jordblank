abcaa75a6607c2ae2c2131dd90b5ff5cd19be575
jordblank <26jblankstein@gannacademy.org>
2025-03-04 13:09:56 -0500

    ghjk

--- src/App.java
+++ src/App.java
@@ -1,12 +1,28 @@
 import processing.core.*;
 
-public class App extends PApplet{
+public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
     }
-
-
+     
+//chat gpt wrote below
+    public void settings(){
+        size(800, 800);
+    }
+    public void setup(){
+        background (232,222,255);
+    }
+    public void draw(){
+        // background(200,150,255);
+        stroke (217, 215, 222);
+        strokeWeight(8);
+        rect (105, 105, 600, 600);
     
-
+    
+    }
 
 }
+
+
+
+
